37827c82875bf315b0091a842e5d539b8817704a
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-01 10:35:40 -0300

    #04.3 - Interagindo com os elementos do DOM

--- 01-introducao-ao-javascript/dio/assets/js/script.js
+++ 01-introducao-ao-javascript/dio/assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+// Interagindo com os elementos do DOM
+document.getElementsByTagName("h1");
+var heading1 = document.getElementsByTagName('h1')[0];
+console.log(heading1);
+
+heading1.style.color = "red";
+console.log(heading1);
