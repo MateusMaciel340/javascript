78ce68603cf830aead12defcb7eb700be2f8888f
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-01 09:26:01 -0300

    #02.1 - Manipulando um arquivo

---
+++ 01-introducao-ao-javascript/01-manipulando-arquivo.js
@@ -0,0 +1,18 @@
+var y = 0;
+var x = 1;
+
+var z = 2;
+
+/* ax + b = 0  */
+
+// Função Afim -> f(1) = a.1 + b
+
+/*
+ * Variáveis: Eles podem variar ao longo do processo ou do seu código. (v)
+ * Constantes: São valores estabelecidos que não podem ser alterados em seguida. (C)
+ */
+
+// var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+var total = PRECO - desconto;
