98c1c49f29f1295d4d1dd509e2b3fd54783192d5
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-01 09:57:41 -0300

    #03.1 - Executando um arquivo.js

---
+++ 01-introducao-ao-javascript/03-executando-arquivo.js
@@ -0,0 +1,27 @@
+var a = 0;
+
+console.log(a);
+
+console.log(a + 1);
+
+console.log("Hello World!");
+
+
+// Retornar números pares
+
+function returnEvenValues(array){
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }else{
+            console.log(`${array[i]} não é par!`);
+        }
+    }
+    console.log("Os números pares são: " 
+        + evenNums);
+}
+
+let array = [1, 2, 4, 5, 7, 8];
+
+returnEvenValues(array);
