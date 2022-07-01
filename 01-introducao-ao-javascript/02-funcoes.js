7a2fb666779e75dc6d2f229042e6f1177a630865
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-01 09:33:53 -0300

    #02.2 - Funções

---
+++ 01-introducao-ao-javascript/02-funcoes.js
@@ -0,0 +1,9 @@
+function soma(a, b){
+    //console.log(a + b);
+    return a + b; // -> 5
+}
+
+soma(3, 5);
+
+// Return -> Quando você for precisa daquele valor em outra situação.
+// Console -> Utilizado normalmente para debugar ou visualizar seu código.
