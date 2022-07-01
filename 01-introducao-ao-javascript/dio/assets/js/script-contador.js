c28d60df075834071af85217db062cc5f5a10695
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-01 13:40:53 -0300

    Desafio 1.2 - stebsnusch (0 <= count =< 10)

--- 01-introducao-ao-javascript/dio/assets/js/script-contador.js
+++ 01-introducao-ao-javascript/dio/assets/js/script-contador.js
@@ -3,9 +3,19 @@ let currentNumber = document.getElementById("currentNumber");
 document.querySelector("#counter #subtrair").addEventListener("click", function(){
     let number = parseInt(currentNumber.textContent) - 1;
     currentNumber.textContent = number;
+
+    if(number <= 0){
+        document.getElementById("subtrair").disabled = true;
+        document.getElementById("subtrair").style.background = "#ccc";
+    }
 });
 
 document.querySelector("#counter #adicionar").addEventListener("click", function(){
     let number = parseInt(currentNumber.textContent) + 1;
     currentNumber.textContent = number;
+
+    if(number >= 10){
+        document.getElementById("adicionar").disabled = true;
+        document.getElementById("adicionar").style.background = "#ccc";
+    }
 });
