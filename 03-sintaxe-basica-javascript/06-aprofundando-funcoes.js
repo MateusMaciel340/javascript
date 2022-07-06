5ec6d4c3f4594b1979be374a8c6d7da02f8057b2
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-06 14:03:45 -0300

    #07.1 - Aprofundando em funções

---
+++ 03-sintaxe-basica-javascript/06-aprofundando-funcoes.js
@@ -0,0 +1,22 @@
+// Tipos funções
+
+// Declarativas
+function Funcao(){
+    console.log("Essa é uma função sendo executada");
+}
+
+Funcao();
+
+// Expressões de funções
+var funcao = function /*funcao*/ (){
+    console.log("Sou uma função sendo executada com uma expressão!");
+}
+
+funcao();
+
+// Arrow Function 
+var funcao = () => {
+    console.log("Sou um array function");
+}
+
+funcao();
