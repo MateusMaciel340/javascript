16637d4c5ddfefc1c63fe60f9898931e8e2be254
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-06 13:53:29 -0300

    #06.1 - Funções e suas particularidades

---
+++ 03-sintaxe-basica-javascript/05-funcoes-particularidades.js
@@ -0,0 +1,12 @@
+function Funcao(){
+    console.log("Essa mensagem chegou através de uma função!");
+}
+
+Funcao();
+
+// Função c/ Parâmetros
+function FuncaoParametro(numero1, numero2){
+    console.log("Os valores atribuidos foram: " + numero1 + " e " + numero2);
+}
+
+FuncaoParametro(2, 3);
