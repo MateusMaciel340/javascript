efbe77fa72ca3c3f5c7a47d46075c965cf5bb5f2
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-07 09:27:56 -0300

    #01.3 - Constantes (Variáveis e Tipos)

---
+++ 04-variaveis-tipos/01-atribuicao-valores/02-constantes.js
@@ -0,0 +1,18 @@
+// Constantes 
+
+/*
+ * Declarada em SNAKE_UPPER_CASE
+ * Escopo de bloco
+ * Não faz hosting 
+ */
+
+const DAYS_IN_A_WEEK = 7;
+const MY_NAME = "Kieran";
+
+console.log(MY_NAME, DAYS_IN_A_WEEK);
+
+// Uma vez declarada, nunca será declarada novamente e nem atribuir um novo valor.
+const FIRST_NAME = "Mateus";
+console.log(FIRST_NAME);
+
+//const LASTNAME -> Não pode não atribuir um valor em uma constante.
