ea9c88752ac45b0bb2a01c791b8e85dfcbaf4795
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-08 13:25:43 -0300

    #01.2 - Modelos Import (Módulos em JavaScript)

---
+++ 05-trabalhando-modulos-javascript/02-import/01-import.js
@@ -0,0 +1,26 @@
+// Importar
+
+/*
+ * Named Exports: import { funcao, variavel, classe } from "./arquivo.js";
+ * Default exports: import valorDefault from "./arquivo.js"
+ */
+
+// Trocando nome de imports
+
+/* import { arquivo as Apelido } from "./arquivo.js";
+   Apelido.metodo();
+
+   import * as INFOS from "./arquivo.js";
+   INFOS.metodo();
+   console.log(INFOS.variavel);
+ */
+
+// Vinculando ao HTML
+
+// <script src="module" src="./main.js"></script>
+
+/*
+ * Para fazer testes localmente (de um arquivo no seu computador), será necesssário
+   estar rodando um servidor. Isso pode ser feito utilizando a extensão
+   "Live Server", do VSCode.
+ */
