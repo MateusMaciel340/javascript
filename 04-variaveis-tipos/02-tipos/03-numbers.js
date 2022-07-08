4fbb6293175d5417bcfaa4031beb075f3681faab
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-08 08:27:45 -0300

    #02.3 - Numbers (Variáveis e Tipos)

---
+++ 04-variaveis-tipos/02-tipos/03-numbers.js
@@ -0,0 +1,24 @@
+// Numbers - números inteiros ou decimais
+
+// Declaração
+let numero = 101;
+console.log(numero);
+
+// Operações matemáticas
+console.log(
+    numero + 2, numero - 2, numero * 2, numero / 2, numero % 2);
+
+// Math
+console.log(typeof Math);
+console.log(Math.PI);
+
+// Conversão
+let fiveByThree = 5 / 3;
+console.log(fiveByThree);
+console.log(Math.floor(fiveByThree));
+console.log(Math.ceil(fiveByThree));
+
+// Porcentagem - Agrupamento de number com string, sem alterar tipos!
+let percent = 10;
+console.log(percent + "%");
+console.log(typeof percent);
