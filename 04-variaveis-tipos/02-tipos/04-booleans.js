90611069ff14d2f37618683b9d20534dea17d800
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-08 08:33:52 -0300

    #02.4 - Booleans (Variáveis e Tipos)

---
+++ 04-variaveis-tipos/02-tipos/04-booleans.js
@@ -0,0 +1,18 @@
+// Booleans -> True ou false
+
+let validation = 3 === 0;
+console.log(validation);
+
+validation = 3 === 3;
+console.log(validation);
+
+validation = 3 > 4;
+console.log(validation);
+
+validation = 3 >= 2;
+console.log(validation);
+
+console.log(validation.toString());
+
+// Negatividade! True para false, ou false para true.
+console.log(!validation);
