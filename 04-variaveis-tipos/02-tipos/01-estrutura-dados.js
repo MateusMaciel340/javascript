944c9ea223b902613bd8109e89d08d37126682aa
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-08 07:49:51 -0300

    #02.1 - Estrutura de dados (Variáveis e Tipos)

---
+++ 04-variaveis-tipos/02-tipos/01-estrutura-dados.js
@@ -0,0 +1,18 @@
+// Estruturas de Dados
+
+/*
+ * JavaScript é uma linguagem de tipagem dinâmica.
+ * Ou seja, antes de declarar um valor, você não específica o tipo dele.
+ */
+
+// String -> Number
+let user_login = "Mateus";
+console.log(typeof user_login);
+
+user_login = 564986;
+console.log(typeof user_login);
+
+/*
+ * Primitive Types: numbers, strings, boolean, null, undefined
+ * Composite/Non-Primitive Types: objects e arrays
+ */
