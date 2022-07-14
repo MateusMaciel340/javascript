85b1f456df88bb2865287406841e7b3a28e0996f
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-14 07:12:37 -0300

    #01.8 - Relação entre tipos de váriaveis (ts-02)

--- 08-pratica-typescript/js/app.js
+++ 08-pratica-typescript/js/app.js
@@ -2,13 +2,19 @@
 var botao = document.getElementById("botao");
 var valor1 = document.getElementById("valor1");
 var valor2 = document.getElementById("valor2");
-function adicionarNumeros(numero1, numero2) {
+function adicionarNumeros(numero1, numero2, devePrintar, frase) {
+    var resultado = numero1 + numero2;
+    if (devePrintar) {
+        console.log(frase + resultado);
+    }
     return numero1 + numero2;
 }
+var devePrintar = true;
+var frase = "O valor é: ";
 if (botao) {
     botao.addEventListener("click", function () {
         if (valor1 && valor2) {
-            console.log(adicionarNumeros(Number(valor1.value), Number(valor2.value)));
+            console.log(adicionarNumeros(Number(valor1.value), Number(valor2.value), devePrintar, frase));
         }
     });
 }
--- 08-pratica-typescript/js/app.ts
+++ 08-pratica-typescript/js/app.ts
@@ -2,15 +2,22 @@ let botao = document.getElementById("botao");
 let valor1 = document.getElementById("valor1") as HTMLInputElement;
 let valor2 = document.getElementById("valor2") as HTMLInputElement;
 
-function adicionarNumeros(numero1: number, numero2: number){
+function adicionarNumeros(numero1: number, numero2: number, devePrintar: boolean, frase: string){
+    let resultado = numero1 + numero2;
+    if(devePrintar){
+        console.log(numero1 + numero2);
+    }
     return numero1 + numero2;
 }
+
+let devePrintar = true;
+let frase = "O valor é: ";
 
 if(botao){
     botao.addEventListener("click", () => {
         if(valor1 && valor2){
             console.log(
-                adicionarNumeros(Number(valor1.value), Number(valor2.value))
+                adicionarNumeros(Number(valor1.value), Number(valor2.value), devePrintar, frase)
             );
         }
     });
