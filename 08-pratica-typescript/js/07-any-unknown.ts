5e039c67a744c5832b3f2497af6c282799673bed
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-14 08:57:29 -0300

    #01.15 - Any e unknown (ts-02)

---
+++ 08-pratica-typescript/js/07-any-unknown.ts
@@ -0,0 +1,19 @@
+let anyEstaDeVolta: any;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = "teste";
+
+let stringTest: string = "verificar";
+stringTest = anyEstaDeVolta;
+
+let unknownValor: unknown;
+unknownValor = 3;
+unknownValor = "opa";
+unknownValor = true;
+unknownValor = "vai sim";
+
+let stringTest2: string = "agora vai";
+//stringTest2 = unknownValor;
+
+if(typeof unknownValor === "string"){
+    stringTest2 = unknownValor;
+}
