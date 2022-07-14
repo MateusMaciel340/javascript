7994d6fc7c7b994bd0eb98e2d6cd34069d7c7aa7
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-14 17:31:28 -0300

    Desafio 01 - TypeScript + JavaScript

---
+++ 09-desafio-typescript/src/desafio-01/desafio-1.js
@@ -0,0 +1,8 @@
+"use strict";
+// Desafio 01
+var employee = {
+    name: "John",
+    code: 10
+};
+employee.name = "Fernando";
+employee.code = 11;
---
+++ 09-desafio-typescript/src/desafio-01/desafio-1.ts
@@ -0,0 +1,9 @@
+// Desafio 01
+
+const employee = {
+    name: "John",
+    code: 10
+};
+
+employee.name = "Fernando";
+employee.code = 11;
