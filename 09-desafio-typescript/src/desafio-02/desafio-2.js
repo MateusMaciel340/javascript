4b1c63c612c7bc75cbd5ea02b5630fe59b9104e7
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-14 17:31:47 -0300

    Desafio 02 - TypeScript + JavaScript

---
+++ 09-desafio-typescript/src/desafio-02/desafio-2.js
@@ -0,0 +1,18 @@
+"use strict";
+// Desafio 02
+var pessoa1 = {
+    nome: "Mateus",
+    idade: 21,
+    profissao: "programador"
+};
+pessoa1.nome = "Fernando";
+pessoa1.idade = 23;
+pessoa1.profissao = "desenvolvedor";
+var pessoa2 = {
+    nome: "Lucas",
+    idade: 19,
+    profissao: "Auxiliar de Produção"
+};
+pessoa2.nome = "Ricardo";
+pessoa2.idade = 21;
+pessoa2.profissao = "Analista de dados";
---
+++ 09-desafio-typescript/src/desafio-02/desafio-2.ts
@@ -0,0 +1,21 @@
+// Desafio 02
+
+const pessoa1 = {
+    nome: "Mateus",
+    idade: 21,
+    profissao: "programador"
+}
+
+pessoa1.nome = "Fernando";
+pessoa1.idade = 23;
+pessoa1.profissao = "desenvolvedor";
+
+const pessoa2 = {
+    nome: "Lucas",
+    idade: 19,
+    profissao: "Auxiliar de Produção"
+}
+
+pessoa2.nome = "Ricardo";
+pessoa2.idade = 21;
+pessoa2.profissao = "Analista de dados";
