1872f1c450605ea642ffaebbcea6a65d093e2f85
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-07-14 08:00:13 -0300

    #01.12 - Retorno de função (ts-02)

---
+++ 08-pratica-typescript/js/04-retorno-funcao.js
@@ -0,0 +1,5 @@
+"use strict";
+function SomarValoresNumericos(numero1, numero2) {
+    return numero1 + numero2;
+}
+console.log(SomarValoresNumericos(1, 2));
---
+++ 08-pratica-typescript/js/04-retorno-funcao.ts
@@ -0,0 +1,5 @@
+function SomarValoresNumericos(numero1: number, numero2: number): number{
+    return numero1 + numero2;
+}
+
+console.log(SomarValoresNumericos(1, 2));
